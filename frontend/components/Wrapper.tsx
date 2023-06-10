d43339f89e09310dda9a7d2b2e8ec58e5a45cae5
edsattar <ahmed.sattar@live.com>
2023-06-10 17:01:52 +0600

    add twmerge and updates

--- frontend/components/Wrapper.tsx
+++ frontend/components/Wrapper.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge";
+
 type Props = {
   children: React.ReactNode;
   className?: string;
@@ -5,7 +7,7 @@ type Props = {
 
 const Wrapper = ({ children, className }: Props) => {
   return (
-    <div className={`mx-auto w-full md:max-w-7xl max-w-lg  px-5 md:px-10 ${className}`}>
+    <div className={twMerge("mx-auto w-full md:max-w-7xl max-w-lg  px-5 md:px-10", className)}>
       {children}
     </div>
   );
